4c95ca5c7a218e29eb1564286965fab5de2bcbf3
Jose Angel <eljosia.rc@gmail.com>
2025-01-21 15:08:46 -0700

    feat: actualizacion de datos, proyectos

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,35 +1,40 @@
 function loadMenu(activo) {
-    $.ajax({
-        type: "GET",
-        url: `./includes/menu.html?v=2`,
-        data: {},
-        success: function (data) {
-            $("#menu").html(data);
-            $(`#menu .${activo}`).addClass("active");
-            $(`#menu .${activo}`).removeAttr('href');
-        }
-    })
+  $.ajax({
+    type: "GET",
+    url: `./includes/menu.html?v=2`,
+    data: {},
+    success: function (data) {
+      $("#menu").html(data);
+      $(`#menu .${activo}`).addClass("active");
+      $(`#menu .${activo}`).removeAttr("href");
+    },
+  });
 }
 function loadpage(pagina) {
-    $('section .load').load(`./pages/${pagina}.html?v=2`).fadeIn(400);
+  $("section .load").load(`./pages/${pagina}.html?v=2`).fadeIn(400);
 }
-function loadFooter(){
-    $('#footer').load(`./includes/footer.html`)
-}
-function loadProyect()
-{
-    var div  = "#load-proyects"
-    var dir  = "./includes/proyects"
-    
-    $.ajax({
-        url: "./includes/scan.php",
-        dataType: 'json',
-        success: function(data) {
-         $.each(data, function(i){
-            $.get(`${dir}/${data[i]}`, function(codigo){
-                $(div).append(codigo);
-            });
-         });
-        }
-       });
-}
+function loadFooter() {
+  $("#footer").load(`./includes/footer.html`);
+};
+
+function loadProyect() {
+  const div = "#load-proyects";
+  const dir = "./includes/proyects";
+
+  let projects = JSON.parse(localStorage.getItem("projects")) || [
+    "biblioteca.html",
+    "chesflix.html",
+    "qikmenu.html",
+  ];
+
+  projects.forEach((file) => {
+    $.get(`${dir}/${file}`)
+      .done((content) => $(div).append(content))
+      .fail((error) => console.error(`Error al cargar ${file}:`, error));
+  });
+};
+
+// Para actualizar manualmente la lista de proyectos:
+const updateProjects = (newProjects) => {
+  localStorage.setItem("projects", JSON.stringify(newProjects));
+};
